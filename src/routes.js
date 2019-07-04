51a39db7ab6d1c5328a2d4468fb7da6507c6b3f9
bruno bisogni <brunobisogni5@gmail.com>
2019-07-04 11:52:07 -0300

    criaçao da pasta middlewar e input do  token por user via jwt ok

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import SessionsController from './app/controller/SessionController';
 
+import authMiddleware from './app/middlewares/Auth';
+
 const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionsController.store);
+
+routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 export default routes;
