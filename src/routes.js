725e4cd5da0549a91a796753db74da2cb3129b05
bruno bisogni <brunobisogni5@gmail.com>
2019-07-03 22:29:09 -0300

    correçao

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Bruno Bisogni',
-    email: 'bruno@bisognidev.xyx',
-    password_hash: '56465456456',
-  });
-  return res.json(User);
+routes.get('/', (req, res) => {
+  return res.json({ message: 'corrigido' });
 });
 
 export default routes;
