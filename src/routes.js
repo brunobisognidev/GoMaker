0fc4b58ecf77789f167b71f485c08cd1debe07e0
bruno bisogni <brunobisogni5@gmail.com>
2019-07-20 00:09:42 -0300

    criaçao de appointments

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,10 @@ import multerConfig from './config/multer';
 import UserController from './app/controller/UserController';
 import SessionsController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controller/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +18,10 @@ routes.post('/sessions', SessionsController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
